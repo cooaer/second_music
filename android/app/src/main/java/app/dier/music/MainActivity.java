c804873cd6d9b99eaf00bdf71d20889c0bc60d59
DESKTOP-F9BLOH9\cooaer <cooaer@outlook.com>
2020-12-04 02:32:03 +0800

    替换MainActivity.kt为MainActivity.java

---
+++ android/app/src/main/java/app/dier/music/MainActivity.java
@@ -0,0 +1,15 @@
+package app.dier.music;
+
+import android.os.Bundle;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
--- android/app/src/main/kotlin/app/dier/music/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package app.dier.music
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-
-}
